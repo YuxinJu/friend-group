fbd9c855ca89c8992bcd1ebfe9ca27bb7fa4febb
Yuxin Ju <yuxin_ju@163.com>
2024-10-14 10:40:56 +0100

    group information

--- group.py
+++ group.py
@@ -2,4 +2,9 @@
 
 # Your code to go here...
 
-my_group =
+my_group =[{ "name": "Jenny", "age": 22, "job": "Student"}, 
+        { "name": "Jason", "age": 22, "job": "Student"},
+        ]
+
+for row in my_group:
+   print (f"{row['name']} is {row['age']},a {row['job']}")
